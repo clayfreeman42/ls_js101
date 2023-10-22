cc0a99762ac3cc5f71fac0fbe51fec90504c0617
Clay Freeman <clayfreeman42@gmail.com>
2023-10-22 02:16:28 -0500

    resolved merge conflicts

--- lesson_2/rps.js
+++ lesson_2/rps.js
@@ -18,7 +18,6 @@ const prompt = msg => console.log(`=> ${msg}`);
 
 // Inputs
 const getPlayerSelection = () => {
-<<<<<<< HEAD
   let playerChoice;
 
   let shortChoices = VALID_CHOICES.map(playerChoice => playerChoice[0]);
@@ -34,22 +33,6 @@ const getPlayerSelection = () => {
   playerChoice = VALID_CHOICES[indexOfChoice];
 
   return playerChoice;
-=======
-  let choice;
-  let shortChoices = VALID_CHOICES.map(choice => choice[0]);
-  prompt(`Choose one (first letter only - case sensitive):\n ${VALID_CHOICES.join(', ')} `);
-  choice = readline.question();
-
-  while (!shortChoices.includes(choice)) {
-    prompt(`Please select the first letter only:\n ${VALID_CHOICES.join(', ')}`);
-    choice = readline.question();
-  }
-
-  let indexOfChoice = shortChoices.indexOf(choice);
-  choice = VALID_CHOICES[indexOfChoice];
-
-  return choice;
->>>>>>> main
 };
 
 const getComputerSelection = () => {
@@ -68,7 +51,6 @@ const getYOrN = response => {
 };
 
 // Outputs
-<<<<<<< HEAD
 const gameReportGenerator = (results, priorGameData) => {
   let { playerChoice, computerChoice, rule, outcome } = results;
 
@@ -100,10 +82,6 @@ const bestOfFiveReportGenerator = (gameInfo) => {
 // Best of five functions
 // keep a maximum of five results from previous games
 const scoreTracker = (scores, result) => {
-=======
-// keep a maximum of five results from previous games
-const updateScores = (scores, result) => {
->>>>>>> main
   if (scores.length < 5) {
     scores.unshift(result);
   } else {
@@ -114,7 +92,6 @@ const updateScores = (scores, result) => {
 };
 
 // count users wins out of number of games, up to five
-<<<<<<< HEAD
 const gameCounter = (arr, data) => {
 
   arr.forEach(game => {
@@ -148,29 +125,6 @@ const calcBestOfFive = gamesArray => {
   };
 
   return gameCounter(gamesArray, gamesData);
-=======
-const calcUserWins = (gameOutcomes) => {
-  let userWinCounter = 0;
-  let gamesPlayed = gameOutcomes.length;
-
-  for (let outcome of gameOutcomes) {
-    if (outcome === `You've won!\n`) {
-      userWinCounter += 1;
-    }
-  }
-  return `You've won ${userWinCounter} game(s) out of the last ${gamesPlayed}.`;
-};
-
-const reportGameResults = (results) => {
-  let { playerChoice, computerChoice, rule, outcome, scoreBoard } = results;
-
-  console.clear();
-
-  prompt(`You chose ${playerChoice}. The computer chose ${computerChoice}.`);
-  prompt(`${rule}\n`);
-  prompt(`${outcome}`);
-  prompt(`${scoreBoard}\n`);
->>>>>>> main
 };
 
 // Main game functions
@@ -183,26 +137,17 @@ const determineWinner = (gameData) => {
 
   for (let index = 0; index < RULES.length; index += 1) {
     if (playerChoice === computerChoice) {
-<<<<<<< HEAD
       gameData.rule = `It's a tie game!`;
-=======
-      gameData.rule = `It's a tie!`;
->>>>>>> main
       gameData.outcome = `Great minds (and programs) think alike!\n`;
       break;
 
     } else if (ruleTest(index, playerChoice, computerChoice)) {
       gameData.rule = RULES[index][2];
-<<<<<<< HEAD
       gameData.outcome = `You've won the game!\n`;
-=======
-      gameData.outcome = `You've won!\n`;
->>>>>>> main
       break;
 
     } else if (ruleTest(index, computerChoice, playerChoice)) {
       gameData.rule = RULES[index][2];
-<<<<<<< HEAD
       gameData.outcome = `The computer won the game.\n`;
       break;
     }
@@ -214,18 +159,6 @@ const determineWinner = (gameData) => {
 let rpsGame = flag => {
   let gameResults = {};
   gameResults.playerChoice = getPlayerSelection(flag);
-=======
-      gameData.outcome = `The computer won.\n`;
-      break;
-    }
-  }
-  return gameData;
-};
-
-let rpsGame = () => {
-  let gameResults = {};
-  gameResults.playerChoice = getPlayerSelection();
->>>>>>> main
   gameResults.computerChoice = getComputerSelection();
   gameResults = determineWinner(gameResults);
 
@@ -233,7 +166,6 @@ let rpsGame = () => {
 };
 
 // Game orchestration
-<<<<<<< HEAD
 const bestOfFiveOrchestrator = (scoresArr) => {
   let bestOfFiveObj = calcBestOfFive(scoresArr);
 
@@ -278,30 +210,3 @@ const firstGamePrompt = bestOfFiveFlag => {
 
   prompt(`Thanks for playing Rock, Paper, Scissors, Lizard, Spock!`);
 })();
-=======
-(() => {
-  let playBestOfFive = '';
-  let scoreKeeper = [];
-
-  do {
-    if (playBestOfFive === '') {
-      console.clear();
-      prompt(`Welcome to Rock, Paper, Scissors, Lizard, Spock!\n`);
-    }
-
-    let game = rpsGame();
-    scoreKeeper = updateScores(scoreKeeper, game.outcome);
-    game.scoreBoard = calcUserWins(scoreKeeper);
-
-    reportGameResults(game);
-
-    prompt(`Best out of five?`);
-    if (playBestOfFive === '') {
-      playBestOfFive = getYOrN();
-    }
-
-  } while (playBestOfFive === 'y' && scoreKeeper.length < 5);
-
-  prompt(`Thanks for playing Rock, Paper, Scissors, Lizard, Spock!`);
-})();
->>>>>>> main
